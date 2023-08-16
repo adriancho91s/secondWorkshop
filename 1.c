7af11b7c1f27ab9081f90fc379a0f8d4c59f2f99
adriancho91s <72105546+adriancho91s@users.noreply.github.com>
2023-08-16 13:11:52 +0000

    Initial commit

---
+++ 1.c
@@ -0,0 +1,34 @@
+/*
+- Fecha: 14 de agosto 2023
+- Hora: 11:30 pm
+- Versión del código: 1.2
+- Presentado por: Adrián Fernando Gaitán Londoño Ing(c)
+- Presentado a: Carlos Alberto Ocampo Sepulveda
+- Lenguaje: C++
+- Version del lenguaje: C++: 4.2.1 
+- Versión del compilador: Apple clang version 14.0.0 (clang-1400.0.29.202)
+- Universidad Tecnológica de Pereira
+*/
+
+//Librerías
+#include <stdio.h>
+
+//Declaración de variables
+
+int limitRange = 5;
+
+int primeNumber (begin, number) {
+  if ((! ((number / begin) === 1)) && !(begin < limitRange)) {
+    
+  }
+}
+
+
+int main(void) {
+  printf("Este programa, lee un número entre uno(1) y cinco(5), e imprime si es primo o no.\n");
+  printf("Salvedad: Para valores fuera de este rango, no garantizamos los resultados.");
+  printf("Entre un número:");
+
+  
+  return 0;
+}
